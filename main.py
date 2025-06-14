90f1adafd84ceb28d74d0d1d424f3cafae6efbca
Matan Azulay <matanazulay123@gmail.com>
2025-06-15 00:51:02 +0300

    Connected to Railway MySQL

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ app = Flask(__name__, template_folder='templates')
 app.secret_key = 'my secret key'
 
 # MySQL database configuration
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'metro.proxy.rlwy.net'
+app.config['MYSQL_PORT'] = 12644
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'dogtrip_db'
+app.config['MYSQL_PASSWORD'] = 'itkkpNkZZrHzwoXrmfuobEQlbIUxSjw'
+app.config['MYSQL_DB'] = 'railway'
+
+
+
+
+
 
 # Mail server configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
